95e29b55ac0f3845168134b58ab838511f39393f
Magnus T.K <magnusdevofficial@gmail.com>
2024-03-21 08:33:50 +0100

    Player Points

---
+++ src/main/java/xyz/zeppelin/casino/bridge/PlayerPointsEconomyBridge.java
@@ -0,0 +1,59 @@
+package xyz.zeppelin.casino.bridge;
+
+import lombok.RequiredArgsConstructor;
+import net.milkbowl.vault.economy.Economy;
+import org.black_ixx.playerpoints.PlayerPoints;
+import org.black_ixx.playerpoints.PlayerPointsAPI;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.ServicesManager;
+import xyz.zeppelin.casino.component.PluginComponent;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@RequiredArgsConstructor
+public class PlayerPointsEconomyBridge implements EconomyBridge, PluginComponent {
+
+    private final Plugin plugin;
+    private PlayerPointsAPI ppApi;
+
+    private int has(Player player) {
+        return ppApi.look(player.getUniqueId());
+    }
+
+    @Override
+    public boolean withdraw(Player player, BigDecimal amount) {
+        if (!(has(player) < amount.intValue())) return false;
+        return ppApi.take(player.getUniqueId(), amount.intValue());
+    }
+
+    @Override
+    public boolean deposit(Player player, BigDecimal amount) {
+        return ppApi.give(player.getUniqueId(), amount.intValue());
+    }
+
+    @Override
+    public boolean hasSufficientBalance(Player player, BigDecimal amount) {
+        return has(player) >= amount.intValue();
+    }
+
+    @Override
+    public BigDecimal getBalance(Player player) {
+        return BigDecimal.valueOf(has(player));
+    }
+
+    @Override
+    public void onEnable() {
+        this.ppApi = PlayerPoints.getInstance().getAPI();
+    }
+
+    public static EconomyBridge detect(Plugin plugin) {
+        try {
+            Class.forName("org.black_ixx.playerpoints.PlayerPoints");
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+        return new PlayerPointsEconomyBridge(plugin);
+    }
+}
